afff86f832dbf8790db050cab80297e2b351c838
Araxiel <fireops@gmail.com>
2018-10-15 19:17:32 -0400

    moved execution into main script file

--- workbench/workbench.py
+++ workbench/workbench.py
@@ -125,26 +125,3 @@ class randomise():
         b = randomise.randomise_color(b_base)
         rgb_value = tuple((r,g,b))
         return rgb_value
-
-## Input
-start_id = 12
-in_culture = "norse"
-in_religion = "catholic"
-in_is_tribal = 0
-in_terrain = "plains"
-rgb_basis = (255, 102, 0)
-
-## Execution
-province.write.init_province_set_up()
-province.write.init_landed_titles()
-spreadsheet = open("provinces.csv", "r")
-current_id = start_id
-provinces = spreadsheet.readlines()
-for x in provinces:
-
-    province_dict = province.read.dictionary_creation(x)
-    province.write.history_province(province_dict, current_id, in_culture, in_religion, in_is_tribal, in_terrain)
-    province.write.history_titles(province_dict)
-    province.write.province_set_up(province_dict, current_id, in_terrain)
-    province.write.landed_titles(province_dict,rgb_basis)
-    current_id += 1
