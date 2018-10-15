910d8226f6638f13278dc242329e638e4a9e0f8c
Araxiel <fireops@gmail.com>
2018-10-15 06:46:33 -0400

    read from .csv file and write multiple files
    on the way

--- source-files/workbench.py
+++ source-files/workbench.py
@@ -19,7 +19,7 @@ class province():
     class write():
         def history_province(province_dict, current_id=6, culture = "norse", religion = "catholic", tribal = 0, terrain = None):
             history_provinces_filename = str(current_id) + " - " + province_dict.get("county").lower()
-            rel_path = "Output\\history\\provinces\\" + history_provinces_filename + ".txt"
+            rel_path = "output\\history\\provinces\\" + history_provinces_filename + ".txt"
             import os
             os.makedirs(os.path.dirname(rel_path), exist_ok=True)
             with open(rel_path, "w") as file:
@@ -27,36 +27,42 @@ class province():
                 file.write("title = c_" + province_dict.get("county").lower() + "\n\n")
                 file.write("# Settlements\nmax_settlements = " + str(len(province_dict.keys())-1) + "\n")
                 if ( tribal == 1 ):
-                    file.write(province_dict.get("barony_1") + " = tribal\n\n")
-                    province.write.comment_overflow_baronies(province_dict,file)
+                    file.write("b_" + province_dict.get("barony_1").lower() + " = tribal\n\n")
+                    province.write.comment_overflow_baronies(province_dict,file,tribal)
                 else:
-                    file.write(province_dict.get("barony_1") + " = castle\n")
-                    file.write(province_dict.get("barony_2") + " = city\n")
-                    file.write(province_dict.get("barony_3") + " = temple\n\n")
+                    file.write("b_" + province_dict.get("barony_1").lower() + " = castle\n")
+                    file.write("b_" + province_dict.get("barony_2").lower() + " = city\n")
+                    file.write("b_" + province_dict.get("barony_3").lower() + " = temple\n\n")
                     province.write.comment_overflow_baronies(province_dict,file)
                 file.write("\n# Misc\nculture = " + culture + "\nreligion = " + religion + "\n")
                 if terrain is not None:
                     file.write("terrain = " + terrain + "\n")
                 file.write("\n# History\n")
 
-        def comment_overflow_baronies(province_dict,file):
-            num = 4
+        def comment_overflow_baronies(province_dict,file,tribal=0):
+            if ( tribal == 1 ):
+                num = 2
+            else:
+                num = 4
             for x in province_dict:
                 province_dict_instance = province_dict.get("barony_" + str(num))
                 if province_dict_instance is None:
                     break
-                file.write("#" + province_dict_instance + "\n")
+                file.write("#b_" + province_dict_instance.lower() + "\n")
                 num += 1
 
-
 ## Input
-in_province_line = "Carcossa;b_carcossa;b_arkham;b_rylhe;b_stratham;x"
-in_start_id = 12
+start_id = 12
 in_culture = "norse"
 in_religion = "catholic"
 in_tribal = 0
+in_terrain = None
 
 ## Execution
-province_dict = province.read.dictionary_creation(in_province_line)
-print(province_dict)
-province.write.history_province(province_dict, in_start_id, in_culture, in_religion, in_tribal, terrain = "steppes")
+spreadsheet = open("provinces.csv", "r")
+current_id = start_id
+provinces = spreadsheet.readlines()
+for x in provinces:
+    province_dict = province.read.dictionary_creation(x)
+    province.write.history_province(province_dict, current_id, in_culture, in_religion, in_tribal, in_terrain)
+    current_id += 1
