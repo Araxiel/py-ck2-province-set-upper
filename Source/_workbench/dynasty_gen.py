f32698cb625aef1ed17d5c21006ea9f0fdca073e
Araxiel <fireops@gmail.com>
2018-10-22 20:37:47 -0400

    dynasty writing scripts
    furthered the dynasty gen scripts, and split up the generator and the writing

--- Source/_workbench/dynasty_gen.py
+++ Source/_workbench/dynasty_gen.py
@@ -1,21 +1,29 @@
-starting_date = list((806, 6, 16))
-
+import dynasty_write
 
 class Character(object):
 
-    def generate_death(self):
+    def generate_death(self, essential=True):
         import random
-        death_ages_container = [random.randint(0, 20)]
+        if essential is False:
+            min_age = 0
+        else:
+            # print(self.name + " es: " + str(essential))
+            min_age = 18
+        death_ages_container = [random.randint(min_age, min_age + 8)]
         for x in range(0, 8):
-            death_ages_container.append(random.randint(20, 60))
+            death_ages_container.append(random.randint(25, 60))
         for x in range(0, 2):
-            death_ages_container.append(random.randint(60, 80))
+            death_ages_container.append(random.randint(65, 80))
+        # print(self.name + " dc: " + str(death_ages_container))
         final_age = random.choice(death_ages_container)
+        # print(self.name + " fa: " + str(final_age))
         death_year = self.birth[0] + final_age
         self.death = daterizer(death_year)
 
     def __init__(self, name=False, dynasty=756, culture='saxon', religion='catholic', birth=(1025, 1, 2),
-                 death=(1066, 10, 14), female=False, game_id=60000):
+                 death=(1066, 10, 14), female=False, game_id=60000, essential=True, main_heir=None):
+        if birth is None:
+            birth = [1025, 1, 2]
         if name is False:
             self.name = get_random_name(female)
         else:
@@ -32,23 +40,31 @@ class Character(object):
         self.spouses_list = []
         self.spouses_marry_dates_list = []
         self.children_list = []
-        self.generate_death()
+        self.essential = essential
+        self.generate_death(essential=essential)
+        self.main_heir = main_heir
+        self.last_child_date = None
 
-    def generate_child(self, female=False):
+    def generate_child(self, female=False, essential=False):
         import random
-        has_child_age = random.randint(16, get_final_age(self.birth, self.death))
-        child_birth_date = daterizer(self.birth[0] + has_child_age)
+        if not self.children_list:
+            print("No Kids")
+            has_first_child_age = random.randint(17, get_age_to_death(self.birth, self.death))
+            child_birth_date = daterizer(self.birth[0] + has_first_child_age)
+        else:
+            print("Already has Kids")
+            var = random.randint(4, get_age_to_death(self.last_child_date, self.death))
+            child_birth_date = daterizer(self.last_child_date[0] + var)
         child_id = assign_id('guys')
         global character_list
-        character_list[child_id] = Character()
+        character_list[child_id] = Character(essential=essential)
         character_list[child_id].birth = child_birth_date
         character_list[child_id].father = self
         character_list[child_id].culture = self.culture
         character_list[child_id].religion = self.religion
-        if len(self.spouses_list) > 0:
+        if len(self.spouses_list):
             for index, item in enumerate(self.spouses_marry_dates_list):
-                print(self.spouses_list[index])
-                if item[0] > child_birth_date[0]:
+                if item[0] < child_birth_date[0]:
                     character_list[child_id].mother = self.spouses_list[index]
                     break
         global global_game_id
@@ -56,18 +72,22 @@ class Character(object):
         character_list[child_id].game_id = global_game_id
         self.children_list.append(character_list[child_id])
         character_list[child_id].female = female
+        global last_char
+        last_char = character_list[child_id]
+        self.last_child_date = child_birth_date
+        return character_list[child_id]
 
-    def generate_wife(self, last_wive_death_date=(0, 0, 0)):
+    def generate_wife(self, last_wive_death_date=None):
         import random
         # If there was no previous wife
-        if last_wive_death_date[0] is 0:
+        if not last_wive_death_date:
             # Will check if person dies before they're 30, so we don't have corpse bride
-            death_age = get_final_age(self.birth, self.death)
-            if death_age > 30:
-                thirty_or_less = 30
+            death_age = get_age_to_death(self.birth, self.death)
+            if death_age > 20:
+                thirty_or_less = 20
             else:
                 thirty_or_less = death_age
-            var1 = random.randint(10, thirty_or_less)
+            var1 = random.randint(10, thirty_or_less + 10)
             marry_year = self.birth[0] + var1
         else:
             marry_year = last_wive_death_date[0] + random.randint(1, 20)
@@ -82,12 +102,20 @@ class Character(object):
         character_list[wife_id].culture = self.culture
         character_list[wife_id].religion = self.religion
         character_list[wife_id].dynasty = 'none'
+        global global_game_id
+        global_game_id += 1
+        character_list[wife_id].game_id = global_game_id
         # father and mother might go here
         self.spouses_list.append(character_list[wife_id])
         self.spouses_marry_dates_list.append(marry_date)
+        character_list[wife_id].spouses_list.append(self)
+        character_list[wife_id].spouses_marry_dates_list.append(marry_date)
+        global last_char
+        last_char = character_list[wife_id]
+        return character_list[wife_id]
 
 
-def daterizer(year_in=starting_date[0]):
+def daterizer(year_in=1025):
     import random
     month = random.randint(1, 12)
     day = random.randint(1, 30)
@@ -95,7 +123,15 @@ def daterizer(year_in=starting_date[0]):
     return date
 
 
-def get_final_age(birth, death):
+# noinspection SpellCheckingInspection
+def wayback(base_year=1025, backtime=100):
+    import random
+    var = random.randint(backtime, backtime + 100)
+    base_year -= var
+    return daterizer(base_year)
+
+
+def get_age_to_death(birth, death):
     age = death[0] - birth[0]
     return age
 
@@ -119,50 +155,50 @@ def get_random_name(female=False):
     return name
 
 
-def gen_new_guy(name=False, dynasty=756, culture='saxon', religion='catholic', birth=(1025, 1, 2), death=(1066, 10, 14),
-                female=False, game_id=60000):
+def generate_character(name=False, dynasty=756, culture='saxon', religion='catholic', birth=(1025, 1, 2), death=None,
+                female=False, game_id=60000, essential=True, founder=False, caller=None):
+    if death is None:
+        death = starting_date
     import random
-    char_number = assign_id('guys')
-    character_list[char_number] = Character(name, dynasty, culture, religion, birth, death, female, game_id)
-    character_list[char_number].generate_wife()
-    for x in range(0, random.randint(1, 8)):
-        character_list[char_number].generate_child()
+    protagonist = caller
+    if founder is True:
+        char_number = assign_id('guys')
+        character_list[char_number] = Character(name=name, dynasty=dynasty, culture=culture, religion=religion, birth=birth,
+                                                death=death, female=female, game_id=game_id, essential=essential)
+        protagonist = character_list[char_number]
+    global last_char
+    last_char = protagonist.generate_wife()
+    last_char = protagonist.generate_child(essential=True)
+    protagonist.main_heir = last_char
+    #TODO amount of potential kids somehow tied to death-date
+    boys_amount = random.randint(0, 1)
+    for x in range(0, boys_amount):
+        last_char = protagonist.generate_child()
+    random_pick = random.choice(protagonist.children_list)
+    random_pick.generate_child()
+    girls_amount_weight = (0, 0, 0, 1, 1, 2)
+    girls_amount = random.choice(girls_amount_weight)
+    for x in range(0, girls_amount):
+        last_char = protagonist.generate_child(female=True)
+    last_char = protagonist
+    return protagonist
 
+
+global_game_id = 86540
+starting_date = list((806, 6, 16))
 
 name_ids = {
-    'guys': 1,
-    'gals': 300,
-    'other': 800
+    'guys': 1000,
+    'gals': 300000,
+    'other': 9000000
 }
-global_game_id = 60000
 character_list = {}
 
-gen_new_guy()
+founder = generate_character(game_id=global_game_id, birth=wayback(starting_date[0], 100),founder=True)
+print(len(founder.children_list))
+last_char = generate_character(caller=founder.main_heir)
+print(last_char.game_id)
+print(len(last_char.children_list))
 
-
-def init_province_set_up():
-    from _workbench import configs
-    config_obj = configs.configs()
-    last_file_id = config_obj.read_config('Last_Setup', 'Last_File_ID')
-    rel_path = "Output\\common\\province_setup\\%(number)s_province_setup.txt" % {'number': last_file_id}
-    import os
-    os.makedirs(os.path.dirname(rel_path), exist_ok=True)
-    open(rel_path, "w")
-    import logging
-    logging.info("Init province-setup %s", rel_path)
-
-
-def province_set_up(province_dict, current_id=6, terrain="plains"):
-    from _workbench import configs
-    config_obj = configs.configs()
-    last_file_id = config_obj.read_config('Last_Setup', 'Last_File_ID')
-    rel_path = "Output\\common\\province_setup\\%(number)s_province_setup.txt" % {'number': last_file_id}
-    import os
-    os.makedirs(os.path.dirname(rel_path), exist_ok=True)
-    with open(rel_path, "a") as file:
-        file.write(str(current_id) + " = {\n")
-        file.write("    title = c_" + province_dict.get("county").lower().replace(" ", "_") + "\n")
-        file.write("    max_settlements = " + str(len(province_dict.keys()) - 1) + "\n")
-        file.write("    terrain = " + terrain + "\n}\n")
-    import logging
-    logging.info("Province-Setup: c_%s", province_dict.get("county").lower().replace(" ", "_"))
+dynasty_write.characters_file_set_up()
+dynasty_write.characters_file_writing(character_list)
---
+++ Source/_workbench/dynasty_write.py
@@ -0,0 +1,44 @@
+def characters_file_set_up():
+    # super basic, will need logging and config and integration and UI and more
+    rel_path = "Output\\history\\characters\\gen_characters.txt"
+    import os
+    os.makedirs(os.path.dirname(rel_path), exist_ok=True)
+    open(rel_path, "w+")
+
+
+def characters_file_writing(char_dictionary):
+    rel_path = "Output\\history\\characters\\gen_characters.txt"
+    import os
+    os.makedirs(os.path.dirname(rel_path), exist_ok=True)
+    with open(rel_path, "a") as file:
+        file.write("# Generated\n")
+        for char in char_dictionary:
+            file.write("%s = {\n" % char_dictionary[char].game_id)
+            file.write("    name = \"%s\"\n" % char_dictionary[char].name)
+            file.write("    dynasty = \"%s\"\n" % char_dictionary[char].dynasty)
+            file.write("    culture = \"%s\"\n" % char_dictionary[char].culture)
+            if char_dictionary[char].father is False:
+                pass
+            else:
+                file.write("    father = %i\n" % int(char_dictionary[char].father.game_id))
+            if char_dictionary[char].mother is False:
+                pass
+            else:
+                file.write("    mother = %i\n" % int(char_dictionary[char].mother.game_id))
+            if char_dictionary[char].female is False:
+                pass
+            else:
+                file.write("    female = yes\n")
+            birth_date = char_dictionary[char].birth
+            file.write("    %s.%s.%s = {\n        birth = yes\n" % (birth_date[0], birth_date[1], birth_date[2]))
+            file.write("    }\n")
+            if len(char_dictionary[char].spouses_list) > 0:
+                for index, spouse in enumerate(char_dictionary[char].spouses_list):
+                    marry_date = char_dictionary[char].spouses_marry_dates_list[index]
+                    file.write("    %s.%s.%s = {\n        add_spouse = %s\n" % (
+                        marry_date[0], marry_date[1], marry_date[2], spouse.game_id))
+                    file.write("    }\n")
+            file.write("    %s.%s.%s = {\n        death = yes\n" % (
+                char_dictionary[char].death[0], char_dictionary[char].death[1], char_dictionary[char].death[2]))
+            file.write("    }\n")
+            file.write("}\n\n")
